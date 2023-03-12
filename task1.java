c7e2728314b2abaf046ac3642b4b41c02d065a8f
omkar jadhav <officialomkarjadhav@gmail.com>
2023-03-12 10:41:01 +0000

    task1 completed

---
+++ task1.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class task1 {
+   public static void main(String[] args) {
+    List<Integer> l=new ArrayList<>();
+    l.add(Integer.valueOf(10));
+    l.add(Integer.valueOf(20));
+
+   for(Integer a:l){
+    System.out.println(a);
+   }
+
+   } 
+}
